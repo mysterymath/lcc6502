cefd4e1167b34da07a78895b5a42b24f062620d9
Daniel Thornburgh <mysterymath@gmail.com>
2018-06-26 23:42:54 -0700

    Parse LCC bytecode for attract.c into a LLVM-like IR graph.

--- mos6502/backend.py
+++ mos6502/backend.py
@@ -1,7 +1,8 @@
-import pprint
+import attr
 import re
 import sys
 from attr import attrs, attrib
+from yapf.yapflib.yapf_api import FormatCode
 
 def read():
     global line
@@ -18,37 +19,60 @@ expect("code")
 expect("proc main 0 0")
 
 @attrs
-class Instruction(object):
-    op = attrib()
-    type_ = attrib()
-    size = attrib()
-    args = attrib(default=None)
+class Store(object):
+    addr = attrib()
+    value = attrib()
 
+@attrs
+class Jump(object):
+    dest = attrib()
 
-arity = {
-    'ADDRG': 0,
-    'CNST': 0,
-    'ASGN': 2,
-    'LABEL': 0,
-    'JUMP': 1,
-}
+@attrs
+class BasicBlock(object):
+    instructions = attrib()
 
-
-stack = []
+labels = {}
+instructions = []
+block = BasicBlock(instructions)
+start = block
 while line != "endproc main 0 0":
     if not line:
         raise "Expected: 'endproc main 0 0'. Found: EOF"
     components = line.split()
     match = re.match("(\w+)(\D)(\d)?", components[0])
-    instruction = Instruction(match[1], match[2], match[3] and int(match[3]))
+    op = match[1]
+    #instruction = Instruction(match[1], match[2], match[3] and int(match[3]))
 
-    args = []
-    for _ in range(arity[instruction.op]):
-        args.append(stack.pop())
-    args.reverse()
-    instruction.args = args
+    if op == 'CNST':
+        instructions.append(int(components[1]))
+    elif op == 'ASGN':
+        value = instructions.pop()
+        addr = instructions.pop()
+        instructions.append(Store(addr, value))
+    elif op == 'LABEL':
+        label = components[1]
+        if label in labels:
+            block = labels[block]
+        else:
+            block = BasicBlock([])
+            labels[label] = block
+        instructions.append(Jump(block))
+        instructions = block.instructions
+    elif op == 'ADDRG':
+        label = components[1]
+        if label not in labels:
+            labels[label] = BasicBlock([])
+        instructions.append(labels[label])
+    elif op == 'JUMP':
+        dest = instructions.pop()
+        instructions.append(Jump(dest))
+        block = BasicBlock([])
+        instructions = block.instructions
 
-    stack.append(instruction)
     read()
 
-pprint.pprint(stack)
+def pprint(x):
+    print(FormatCode(repr(x))[0])
+
+print("Start:")
+pprint(start)
