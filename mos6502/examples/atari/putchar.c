1a9c5a9f0e62f90b2e0c671f20d3cbb720846129
Daniel Thornburgh <mysterymath@gmail.com>
2018-07-06 10:50:11 -0700

    Add putchar C version

---
+++ mos6502/examples/atari/putchar.c
@@ -0,0 +1,28 @@
+#define IOCB0 (*(IOCB*)$0340)
+#define PUT_CHARACTERS 0x0B
+#define CIOV ((void*)0xE456)
+
+void __ldx(unsigned char x);
+void __lda(unsigned char a);
+void __jsr(void* destination);
+
+struct IOCB {
+  unsigned char HID;
+  unsigned char DNO;
+  unsigned char CMD;
+  unsigned char STA;
+  void* BA; 
+  void* PT;
+  unsigned short BL;
+  unsigned char AX[6];
+};
+
+void main(void) {
+  IOCB0.CMD = PUT_CHARACTERS;
+  IOCB0.BA = 0;
+  __ldx(0);
+  __lda('A');
+  __jsr(CIOV);
+  while(1)
+    ;
+}
