b6ae338a5921e2a25cdb5ab7b835f5546ee5159b
Daniel Thornburgh <mysterymath@gmail.com>
2018-11-30 23:18:06 -0800

    Fully parenthesize mult-token limits just in case.

--- include/limits.h
+++ include/limits.h
@@ -4,10 +4,10 @@
 #define CHAR_BIT 8
 
 /* Minimum value for an object of type signed char. */
-#define SCHAR_MIN -127
+#define SCHAR_MIN (-127)
 
 /* Maximum value for an object of type signed char. */
-#define SCHAR_MAX +127
+#define SCHAR_MAX 127
 
 /* Maximum value for an object of type unsigned char. */
 #define UCHAR_MAX 255
@@ -22,28 +22,28 @@
 #define MB_LEN_MAX 1
 
 /* Minimum value for an object of type short int. */
-#define SHRT_MIN -32767
+#define SHRT_MIN (-32767)
 
 /* Maximum value for an object of type short int. */
-#define SHRT_MAX +32767
+#define SHRT_MAX 32767
 
 /* Maximum value for an object of type unsigned short int. */
 #define USHRT_MAX 65535
 
 /* Minimum value for an object of type int. */
-#define INT_MIN -32767
+#define INT_MIN (-32767)
 
 /* Maximum value for an object of type int. */
-#define INT_MAX +32767
+#define INT_MAX 32767
 
 /* Maximum value for an object of type unsigned int. */
 #define UINT_MAX 65535
 
 /* Minimum value for an object of type long int. */
-#define LONG_MIN -2147483647
+#define LONG_MIN (-2147483647)
 
 /* Maximum value for an object of type long int. */
-#define LONG_MAX +2147483647
+#define LONG_MAX 2147483647
 
 /* Maximum value for an object of type unsigned long int. */
 #define ULONG_MAX 4294967295
