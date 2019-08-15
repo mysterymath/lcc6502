7d5c3902ce25df7961a698b37ace38e767de5506
Daniel Thornburgh <mysterymath@gmail.com>
2019-08-14 21:24:32 -0700

    Parse ASMs. Parsing complete, but need to refactor.

--- prototype/alpha/alpha.py
+++ prototype/alpha/alpha.py
@@ -69,6 +69,40 @@ class Cmd:
         return f'{results_str}{self.op} {" ".join(self.args)}\n'
 
 
+@attrs
+class Asm:
+    inputs = attrib()
+    clobbers = attrib()
+    instrs = attrib()
+
+    def __str__(self):
+        begin_str = 'asm\n'
+        inputs_str = textwrap.indent(''.join(map(str, self.inputs)), '  ')
+        clobbers_str = textwrap.indent(f'clobbers {" ".join(map(str, self.clobbers))}\n', '  ')
+        instrs_str = textwrap.indent(''.join(map(str, self.instrs)), '  ')
+        end_str = 'end\n'
+
+        return ''.join([begin_str, inputs_str, clobbers_str, instrs_str, end_str])
+
+
+@attrs
+class AsmInstr:
+    op = attrib()
+    args = attrib()
+
+    def __str__(self):
+        return f'{self.op} {" ".join(map(str, self.args))}\n'
+
+
+@attrs
+class AsmInput:
+    register = attrib()
+    value = attrib()
+
+    def __str__(self):
+        return f'input {self.register} {self.value}\n'
+
+
 def read_lines():
     global debug_line
     for line in fileinput.input():
@@ -112,12 +146,12 @@ def parse_func(first, rest):
     return Func(name, inputs, outputs, blocks)
 
 
-def parse_input(first, typ):
+def parse_input(first, rest):
     (_, name, size) = first.split()
     return Input(name, size)
 
 
-def parse_output(first, typ):
+def parse_output(first, rest):
     (_, name, size) = first.split()
     return Output(name, size)
 
@@ -141,10 +175,14 @@ def parse_block(first, rest):
 def parse_cmds(first, rest):
     cmds = []
     while True:
-        cmd = parse_cmd(first, rest)
-        cmds.append(cmd)
-        if cmd.is_terminator():
-            return cmds
+        if first == 'asm':
+            cmd = parse_asm(first, rest)
+            cmds.append(cmd)
+        else:
+            cmd = parse_cmd(first, rest)
+            cmds.append(cmd)
+            if cmd.is_terminator():
+                return cmds
         first = next(rest)
 
 
@@ -153,6 +191,47 @@ def parse_cmd(first, rest):
     results = results.split() if results else []
     (op, *args) = expr.split()
     return Cmd(results, op, args)
+
+
+def parse_asm(first, rest):
+    first = next(rest)
+
+    inputs = []
+    clobbers = []
+    while first.split()[0] == 'input':
+        inputs.append(parse_asm_input(first, rest))
+        first = next(rest)
+    if first.split()[0] == 'clobber':
+        clobbers = parse_clobber(first, rest)
+        first = next(rest)
+
+    instrs = parse_asm_instrs(first, rest)
+
+    return Asm(inputs, clobbers, instrs)
+
+
+def parse_asm_input(first, rest):
+    (_, register, value) = first.split()
+    return AsmInput(register, value)
+
+
+def parse_clobber(first, rest):
+    return list(first.split()[1:])
+
+
+def parse_asm_instrs(first, rest):
+    instrs = []
+    while True:
+        instrs.append(parse_asm_instr(first, rest))
+        first = next(rest)
+        if first == 'end':
+            return instrs
+
+
+def parse_asm_instr(first, rest):
+    (op, *args) = first.split()
+    return AsmInstr(op, args)
+
 
 try:
     funcs = parse()
