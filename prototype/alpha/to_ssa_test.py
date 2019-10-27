2f67e3ef1ac1bdea153b29742a56856a8168e07c
Daniel Thornburgh <mysterymath@gmail.com>
2019-10-27 10:07:26 -0700

    Test basic to_ssa renumbering.

--- prototype/alpha/to_ssa_test.py
+++ prototype/alpha/to_ssa_test.py
@@ -1,3 +1,5 @@
+from to_ssa import to_ssa
+
 import unittest
 from textwrap import dedent
 
@@ -5,16 +7,23 @@ from parse import parse_lines
 
 
 class TestToSsa(unittest.TestCase):
-  def test_basic_parse(self):
+  def test_renumbers_defns(self):
     (func,) = parse_lines("""
       main
         start
+          a = add 1 2
+          b = add 3 4
+          a = add 5 6
           ret
       end
     """)
+    to_ssa(func.blocks)
     self.assertMultiLineEqual(str(func), dedent("""\
       main
         start
+          a = add 1 2
+          b = add 3 4
+          a1 = add 5 6
           ret
       end
     """))
